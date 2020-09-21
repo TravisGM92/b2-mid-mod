d64e0f6300d807ce7096c72457fc0e5cb5ecb048
Travis McKinstry <mckinstrytg@gmail.com>
2020-09-21 10:44:58 -0600

    Create view, model and controller for airline, plus a show medthod for airline

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,4 +1,9 @@
 class Airline < ApplicationRecord
   validates_presence_of :name
   has_many :flights
+
+  def unique_passengers
+    flight_ids = Flight.where(airline_id: self.id).pluck(:id)
+    passengers = Passenger.joins(:flights).where(flights: {id: flight_ids}).pluck(:name).uniq
+  end
 end
