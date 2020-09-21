a068afd3dfa0f1f965b6ea7f631da8505889ba81
Travis McKinstry <mckinstrytg@gmail.com>
2020-09-21 11:14:14 -0600

    Create link to remove a passenger from a flight, created method 'destroy' for flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,4 +3,10 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
   end
+
+  def destroy
+    pass = Passenger.find(params[:id])
+    PassengerFlight.where(passenger_id: params[:id])[0].destroy
+    redirect_to("/flights")
+  end
 end
