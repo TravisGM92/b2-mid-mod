42b6c81e9d9c08224185536f9818623f91112384
Travis McKinstry <mckinstrytg@gmail.com>
2020-09-21 11:32:37 -0600

    Create 'number of flights' method

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,8 @@ class Passenger < ApplicationRecord
   has_many :passenger_flights
   has_many :flights, through: :passenger_flights
   validates_presence_of :name, :age
+
+  def number_of_flights
+    Flight.joins(:passengers).where(passengers: {id: self.id}).count
+  end
 end
